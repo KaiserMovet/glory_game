a1fc8e0b2070fd7bdcc0c4225229886d90b042c0
Mateusz Rynkiewicz <rynkiewiczmate@gmail.com>
2022-11-16 13:41:37 +0100

    Added progress bar to cards

--- app/static/card.js
+++ app/static/card.js
@@ -25,17 +25,30 @@ class Card {
             template.firstElementChild.setAttribute("onclick", `Game.buyCard('${this.obj_id}')`)
         }
         template.firstElementChild.id = this.obj_id;
-        template.getElementById("color").innerHTML = this.color
+        // template.getElementById("color").innerHTML = this.color
         template.getElementById("value").innerHTML = this.value
         for (const [color, cost] of Object.entries(this.cost)) {
-            template.getElementById(`cost_${color}`).innerHTML = cost.toString()
+            // template.getElementById(`cost_${color}`).innerHTML = cost.toString()
+            if (cost == '0') {
+                template.getElementById(`cost_${color}`).parentNode.hidden = true
+                continue
+            }
+            template.getElementById(`cost_${color}`).setAttribute('aria-valuemax', cost);
+            template.getElementById(`cost_${color}`).innerHTML = cost;
+
             if (player != null) {
                 let score = player.getColorScore(color)
-                if (score < cost) {
-                    template.getElementById(`cost_${color}`).innerHTML += ` (${score - cost})`
-                }
+                template.getElementById(`cost_${color}`).setAttribute('aria-valuenow', score);
+                template.getElementById(`cost_${color}`).setAttribute('style', `width: ${score * 100 / cost}%`);
+                if (cost > score) template.getElementById(`cost_${color}`).innerHTML = `${score}/${cost}`;
+
+
+                // if (score < cost) {
+                //     template.getElementById(`cost_${color}`).innerHTML += ` (${score - cost})`
+                // }
             }
         }
+        console.log(template)
         return template
     }
 
