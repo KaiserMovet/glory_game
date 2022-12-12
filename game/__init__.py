5af6261ade31da56d9b1b7378a4d147e753bca85
Mateusz Rynkiewicz <rynkiewiczmate@gmail.com>
2022-12-12 02:52:37 +0100

    odl images

--- game/__init__.py
+++
@@ -1,7 +0,0 @@
-from game.logger import init
-
-from .card import Card
-from .color import Color
-from .game import Game, IllegalMove
-
-init()
--- game/color.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, auto
-
-
-class Color(str, Enum):
-    white = "white"
-    red = "red"
-    green = "green"
-    blue = "blue"
-    black = "black"
